c6ec6eeb63fbbaba9d6d3c33b17ef02f5ecb0695
Ibrar Ahmad <ibrarahmadkhan09@gmail.com>
2026-06-01 11:59:48 +0300

    Fix: Finalized POM dependencies and fixed startup annotations

--- src/main/java/com/shopsphere/store_service/StoreServiceApplication.java
+++ src/main/java/com/shopsphere/store_service/StoreServiceApplication.java
@@ -3,17 +3,14 @@ package com.shopsphere.store_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableJpaAuditing
-@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@EnableSpringDataWebSupport
 public class StoreServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StoreServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+       SpringApplication.run(StoreServiceApplication.class, args);
+    }
 }
